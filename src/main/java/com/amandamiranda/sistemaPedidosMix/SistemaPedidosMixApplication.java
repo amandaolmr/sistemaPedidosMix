34709776b7b4ab84f98aa00bf0aba448c1057346
Amanda Oliveira <amandinha.g.2009@hotmail.com>
2021-01-11 22:27:48 -0300

    Projeto Criado

---
+++ src/main/java/com/amandamiranda/sistemaPedidosMix/SistemaPedidosMixApplication.java
@@ -0,0 +1,13 @@
+package com.amandamiranda.sistemaPedidosMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidosMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidosMixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amandamiranda/cursomc/SistemaPedidosMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amandamiranda.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
